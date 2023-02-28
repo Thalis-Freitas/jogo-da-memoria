0b4b0c5d06d8c4c0f7c0e8d9a8fee9691c0669fd
Thalis Freitas <ofthalis@gmail.com>
2023-02-28 14:12:38 -0300

    assemble board

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,14 @@
 const FRONT = 'card-front'
 const BACK = 'card-back'
+const CARD = 'card'
+const ICON = 'icon'
 
 let techs = ['bootstrap',
-             'css',
+             'css3',
              'docker',
              'git',
              'github',
-             'html',
+             'html5',
              'javascript',
              'postgres',
              'rails',
@@ -20,24 +22,14 @@ startGame()
 function startGame(){
   let cards = createCardsFromTechs(techs)
   shuffleCards(cards)
-  console.log(cards)
-}
-
-function shuffleCards(cards){
-  let currentIndex = cards.length
-  let randomIndex = 0
-  while(currentIndex !== 0){
-    randomIndex = Math.floor(Math.random() * currentIndex)
-    currentIndex--
-    [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]]
-  }
+  initializeCards(cards)
 }
 
 function createCardsFromTechs(techs){
   let cards = []
-  for(let tech of techs){
+  techs.forEach((tech) => {
     cards.push(createPairFromTech(tech))
-  }
+  })
   return cards.flatMap(pair => pair)
 }
 
@@ -54,4 +46,50 @@ function createPairFromTech(tech){
 
 function createIdForTech(tech){
   return tech + parseInt(Math.random() * 1000)
+}
+
+function shuffleCards(cards){
+  let currentIndex = cards.length
+  let randomIndex = 0
+  while(currentIndex !== 0){
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex--
+    [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]]
+  }
+}
+
+function initializeCards(cards){
+  let gameBoard = document.querySelector('#game-board')
+  cards.forEach((card) => {
+    let cardElement = document.createElement('div')
+    cardElement.id = card.id
+    cardElement.classList.add(CARD)
+    cardElement.dataset.icon = card.icon
+    createCardContent(card, cardElement)
+    cardElement.addEventListener('click', flipCard)
+    gameBoard.appendChild(cardElement)
+  })
+}
+
+function createCardContent(card, cardElement){
+  createCardFace(FRONT, card, cardElement)
+  createCardFace(BACK, card, cardElement)
+}
+
+function createCardFace(face, card, element){
+  let cardElementFace = document.createElement('div')
+  cardElementFace.classList.add(face)
+  if(face === FRONT){
+    let iconElement = document.createElement('img')
+    iconElement.classList.add(ICON)
+    iconElement.src = `./assets/images/${card.icon}.svg`
+    cardElementFace.appendChild(iconElement)
+  }else{
+    cardElementFace.innerHTML = '&lt/&gt'
+  }
+  element.appendChild(cardElementFace)
+}
+
+function flipCard(){
+  this.classList.add('flip')
 }
